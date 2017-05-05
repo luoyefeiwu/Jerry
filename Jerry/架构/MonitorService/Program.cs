9140bc90e36fe22463ff014796349ea18462bb07
jerry <568384493@qq.com>
2017-05-05 17:47:26 +0800

    Client Send WebSocket

---
+++ Jerry/架构/MonitorService/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace MonitorService
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            //ThreadPool.QueueUserWorkItem(new WaitCallback(Test), "ws://192.168.1.10:2012");
+            //ThreadPool.QueueUserWorkItem(new WaitCallback(Test), "ws://192.168.1.19:2012");
+
+            WebSocketHelper socket = new WebSocketHelper("ws://192.168.1.10:2012");
+            socket.Connection();
+            Timer timer = new Timer(new TimerCallback(TestTimer), socket, 1000, 1000);
+            
+       
+
+            Console.Read();
+        }
+
+
+        static void TestTimer(object o)
+        {
+            WebSocketHelper socket = o as WebSocketHelper;
+            string sendStr = "heart {\"message_method\":\"heart\",\"message_queue\":\"heart\"}" + "\r\n";
+            var str = socket.Send(sendStr);
+            Console.WriteLine(DateTime.Now.ToString() + str);
+            System.Threading.Thread.Sleep(10);
+        }
+
+
+
+
+
+        private static void Test(object o)
+        {
+            string url = o as string;
+
+            WebSocketHelper socket = new WebSocketHelper(url);
+            //WebSocketHelper socket = new WebSocketHelper("ws://192.168.1.10:2012");
+            socket.Connection();
+            string sendStr = "heart {\"message_method\":\"heart\",\"message_queue\":\"heart\"}" + "\r\n";
+
+            while (true)
+            {
+                var str = socket.Send(sendStr);
+                Console.WriteLine(DateTime.Now.ToString() + str);
+                System.Threading.Thread.Sleep(10);
+            }
+        }
+
+
+    }
+}
---
+++ Jerry/架构/MonitorService/WebSocketHelper.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using WebSocket4Net;
+
+namespace MonitorService
+{
+    public class WebSocketHelper
+    {
+        private WebSocket websocket;
+
+        private string message = string.Empty;
+        private AutoResetEvent _messageReceivedEvent = new AutoResetEvent(false);
+        private string url = string.Empty;
+       
+
+        public WebSocketHelper(string url)
+        {
+            this.url = url;
+        }
+
+        public void Connection()
+        {
+            websocket = new WebSocket4Net.WebSocket(url, "basic", WebSocketVersion.Rfc6455);
+            websocket.Opened += websocket_Opened;
+            websocket.Error += websocket_Error;
+            websocket.Closed += websocket_Closed;
+            websocket.MessageReceived += websocket_MessageReceived;
+            //websocket.Open();
+        }
+
+        public string Send(string msg)
+        {
+
+            switch (websocket.State)
+            {
+                case WebSocketState.Closed:
+                    Connection();
+                    websocket.Open();
+                    _messageReceivedEvent.WaitOne();
+                    break;
+                case WebSocketState.Closing:
+                    //Connection();
+                    //websocket.Open();
+                    //_messageReceivedEvent.WaitOne();
+                    break;
+                case WebSocketState.Connecting:
+                    break;
+                case WebSocketState.None:
+                    websocket.Open();
+                    _messageReceivedEvent.WaitOne();
+                    break;
+                case WebSocketState.Open:
+                    break;
+                default:
+                    break;
+            }
+
+            websocket.Send(msg);
+            _messageReceivedEvent.WaitOne();
+            return message;
+        }
+
+        void websocket_MessageReceived(object sender, MessageReceivedEventArgs e)
+        {
+            message = e.Message;
+            this._messageReceivedEvent.Set();
+        }
+
+        void websocket_Closed(object sender, EventArgs e)
+        {
+            message = "cloased";
+            this._messageReceivedEvent.Set();
+        }
+
+        void websocket_Error(object sender, SuperSocket.ClientEngine.ErrorEventArgs e)
+        {
+            message = "error";
+            this._messageReceivedEvent.Set();
+        }
+
+        void websocket_Opened(object sender, EventArgs e)
+        {
+            this._messageReceivedEvent.Set();
+        }
+
+    }
+}
