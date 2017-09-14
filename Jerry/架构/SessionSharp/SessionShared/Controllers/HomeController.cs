e9b6e44cae32549584931b38907922f766b9dc73
jerry <568384493@qq.com>
2017-09-14 19:19:03 +0800

    Session 分布式共享

---
+++ Jerry/架构/SessionSharp/SessionShared/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SessionShared.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            if (this.HttpContext.Session["MySession"] == null)
+            {
+                this.HttpContext.Session["MySession"] = "我是Session你猜对了吗";
+            }
+            return View();
+        }
+        public ActionResult GetSession()
+        {
+            if (this.HttpContext.Session["MySession"] != null)
+            {
+                ViewBag.MySession = this.HttpContext.Session["MySession"].ToString();
+                ViewBag.SessionID = this.HttpContext.Session.SessionID;
+            }
+            return View();
+        }
+    }
+}
